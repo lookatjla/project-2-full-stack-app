f8004ebd28ab1512870aa3121223606b501db3e0
Jayla Young <jaylayoung1@gmail.com>
2022-12-10 22:27:21 -0600

    server is running, database is connected

--- server.js
+++ server.js
@@ -1,31 +1,29 @@
-// import
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const methodOverride = require('method-override')
-const mongoose = require('mongoose')
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
 
+const DATABASE_URL = 'mongodb+srv://lookatjla:jAshDaGod3ss!@sei.pwit4m3.mongodb.net/homemade?retryWrites=true&w=majority'
 
-// create app
-const app = express()
+const db = mongoose.connection
 
+mongoose.connect(DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}); // tells our mongoose to connect to our database
 
-// register my middleware
-app.use(morgan('dev'))
-app.use('/static', express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
-
+// database connection error/success
+db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
 
 // routes and routers
 app.get('/', (req, res) => {
     res.send('<h1>server is working</h1>')
-})
+});
 
 
 // start the server (listener)
 const PORT = process.env.PORT || 3007
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
-})
-
+});
